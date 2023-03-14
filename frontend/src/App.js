58db9d81fbdf3463eca7bf9e0775e9cbfeff78a2
Михаил Савченко <mihail@MacBook-Pro-Mihail.local>
2023-03-14 18:39:53 +0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+import axios from 'axios'
+
+function App() {
+
+  const url = "http://127.0.0.1:8000/";
+
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    axios.get(url)
+    .then(res => setDatas(res.data))
+    .catch(err => console.log(err));
+  }, [])
+
+  return (
+    <div className="App">
+      <pre className='json'>{JSON.stringify(datas,null, 2)}</pre>
+      <h1>HeLLO</h1>
+    </div>
+  );
+}
+
+export default App;
